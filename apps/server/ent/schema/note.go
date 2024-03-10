eca93691d79e32c726fa6b9c64404bea775e21d7
expesh <104657457+expesh@users.noreply.github.com>
2024-03-10 02:26:36 +0200

    feat(server): add initial ent setup

---
+++ apps/server/ent/schema/note.go
@@ -0,0 +1,37 @@
+package schema
+
+import (
+	"time"
+
+	"entgo.io/contrib/entgql"
+	"entgo.io/ent"
+	"entgo.io/ent/schema"
+	"entgo.io/ent/schema/field"
+)
+
+// Note holds the schema definition for the Note entity.
+type Note struct {
+	ent.Schema
+}
+
+// Fields of the Note.
+func (Note) Fields() []ent.Field {
+	return []ent.Field{
+		field.String("title"),
+		field.Text("content"),
+		field.Time("created_at").
+			Default(time.Now),
+	}
+}
+
+// Edges of the Note.
+func (Note) Edges() []ent.Edge {
+	return nil
+}
+
+func (Note) Annotations() []schema.Annotation {
+	return []schema.Annotation{
+		entgql.QueryField(),
+		entgql.Mutations(entgql.MutationCreate()),
+	}
+}
---
+++ apps/server/resolver.go
@@ -0,0 +1,16 @@
+package notebook
+
+import (
+	"github.com/99designs/gqlgen/graphql"
+	"github.com/altierawr/notebook/ent"
+)
+
+// Resolver is the resolver root.
+type Resolver struct{ client *ent.Client }
+
+// NewSchema creates a graphql executable schema.
+func NewSchema(client *ent.Client) graphql.ExecutableSchema {
+	return NewExecutableSchema(Config{
+		Resolvers: &Resolver{client},
+	})
+}
