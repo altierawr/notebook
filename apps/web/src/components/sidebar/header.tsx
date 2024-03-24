ff67544a0ad705a6335c8dd0718876d9445f3cf5
expesh <104657457+expesh@users.noreply.github.com>
2024-03-24 14:56:23 +0200

    chore(web): remove search icon

--- apps/web/src/components/sidebar/header.tsx
+++ apps/web/src/components/sidebar/header.tsx
@@ -1,7 +1,6 @@
 import {
   IconFolderPlus,
   IconPencilPlus,
-  IconSearch,
   IconSettings,
   IconSortAscending,
 } from "@tabler/icons-react"
@@ -11,9 +10,6 @@ const Header = () => {
   return (
     <div className="text-gray-11 flex h-9 w-full items-center justify-between px-4">
       <div className="flex items-center gap-3">
-        <IconButton color="slate" variant="ghost">
-          <IconSearch size={20} stroke={1.5} />
-        </IconButton>
         <IconButton color="slate" variant="ghost">
           <IconPencilPlus size={20} stroke={1.5} />
         </IconButton>
