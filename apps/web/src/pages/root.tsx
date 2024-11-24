0eb99f5a2b06efbc3d2e663e98fc6127f2036470
altierawr <104657457+expesh@users.noreply.github.com>
2024-11-24 18:13:58 +0200

    fix(web): set app location based on path on initial load

--- apps/web/src/pages/root.tsx
+++ apps/web/src/pages/root.tsx
@@ -1,8 +1,33 @@
-import { Outlet } from "react-router-dom";
+import { Outlet, useLocation } from "react-router-dom";
 import NotesList from "../components/notes-list";
 import Sidebar from "../components/sidebar";
+import { useEffect, useState } from "react";
+import { useLocationStore } from "../store";
 
 const Root = () => {
+  const location = useLocation();
+  const setLocation = useLocationStore((state) => state.setLocation);
+  const [hasSetInitialLocation, setHasSetInitialLocation] = useState(false);
+
+  useEffect(() => {
+    if (hasSetInitialLocation) {
+      return;
+    }
+
+    setHasSetInitialLocation(true);
+
+    switch (location.pathname) {
+      case "/favorites":
+        setLocation("favorites");
+        break;
+      case "/trash":
+        setLocation("trash");
+        break;
+      default:
+        setLocation("notes");
+    }
+  }, [hasSetInitialLocation, location.pathname, setLocation]);
+
   return (
     <div className="w-full flex h-[100dvh]">
       <Sidebar />
